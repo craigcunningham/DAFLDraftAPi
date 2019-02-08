acc4a74096741365fb5f47b8607ad48198eb1634
Craig Cunningham <clcunnin@yahoo.com>
2019-02-07 22:19:09 -0500

    Changed Model Names

--- app/Models/Team.php
+++ app/Models/Team.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class DociTeam extends Model
+class Team extends Model
 {
     /**
      * The attributes that are mass assignable.
